127a72a5b52a624fa5ce86119090f5ecaed63282
enginehenryed <enginehenryed@gmail.com>
2017-03-05 18:32:17 +0900

    Add Webpack and configuration

---
+++ app/components/Power.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+// export interface HelloProps { compiler: string; framework: string; }
+// interface MyState {}
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the 'undefined' type.
+class Power extends React.Component {
+  render() {
+    return (
+      <div>
+        123
+      </div>
+    )
+  }
+}
+
+export default Power
---
+++ internals/webpack/webpack.prod.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    filename: "bundle.js",
+    path: './dist'
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", '.jsx']
+  },
+
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.js$/, // Transform all .js files required somewhere with Babel
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ],
+
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
+};
+
+module.exports = config;
