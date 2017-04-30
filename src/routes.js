4643910b4afa877fef6af7df9cbfd1b83c6b1dc6
imcheck <sungwon520@gmail.com>
2017-05-01 00:15:09 +0900

    wip: Edit routes.js after deleting AppContainer dir

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Route } from "react-router-dom"
+import {BrowserRouter as Router, Route } from "react-router-dom";
 
-import AppContainer from './containers/AppContainer/AppContainer'
+import MainContainer from './containers/MainContainer/MainContainer';
 
-
-const Foo = () => <div>foo0505</div>
-const Bar = () => <div>bar123</div>
 
 export default
   <Router>
     <div>
-      <Route exact path="/" component={AppContainer}/>
-      <Route path="/about" component={Bar}/>
+      <Route exact path="/" component={MainContainer}/>
     </div>
   </Router>
-
-
-
-// <Route exact path="/"><Power/></Route>
-//   <Route path="/test"><div className={styles.temp}>p1ower23</div></Route>
