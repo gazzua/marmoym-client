386d80dfb6544f882b81a3df27bf102d2494b1ab
enginehenryed <enginehenryed@gmail.com>
2017-07-16 17:13:45 +0900

    Add .tsx files

---
+++ src/components/Temp.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export default class Temp extends React.Component<{}, {}> {
+
+  render() {
+    return (
+      <div>
+        powerabc111
+      </div>
+    )
+  }
+}
---
+++ src/containers/MainContainer/MainContainer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import CommonLayout from '../../components/CommonLayout/CommonLayout';
+
+class MainContainer extends React.Component<{}, {}> {
+
+  render() {
+    return <CommonLayout/>
+  }
+
+}
+
+export default MainContainer;
