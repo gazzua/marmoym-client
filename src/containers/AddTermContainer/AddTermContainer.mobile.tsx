7de937435eba7e414f27d9a330fd8c6151ee5170
enginehenryed <enginehenryed@gmail.com>
2017-09-10 14:30:16 +0900

    Add a functional button to append `usage` inputs
    - 쓰임새 추가 버튼 클릭시 쓰임새 인풋엘리먼트가 동적으로 생성됨.
    - UI 가 조금 변경됨.

--- src/containers/AddTermContainer/AddTermContainer.mobile.tsx
+++ src/containers/AddTermContainer/AddTermContainer.mobile.tsx
@@ -3,17 +3,42 @@ import { withRouter } from 'react-router-dom';
 
 import AddTerm from '../../components/AddTerm/AddTerm';
 
-class MastheadContainer extends React.Component {
-
-  constructor() {
-    super()
+namespace AppContainer {
+  export interface Props {
+    masthead?: typeof AddTermContainer,
+    pageContent?: any,
   }
 
+  export interface State {
+    usages: any
+  }
+}
+interface AppContainerProps {
+  
+}
+
+class AddTermContainer extends React.Component<AppContainer.Props, AppContainer.State> {
+
+  constructor() {
+    super();
+    this.state = {
+      usages : [
+        ''
+      ]
+    };
+  }
+  handleClickAddUsage() {
+    let { usages }=this.state;
+    usages.push('');
+    this.setState({usages: usages});
+  }
   render() {
     return (
-      <AddTerm/>
+      <AddTerm 
+        usages={this.state.usages} 
+        handleClickAddUsage={this.handleClickAddUsage.bind(this)}/>
     )
   }
 }
 
-export default withRouter(MastheadContainer);
+export default withRouter(AddTermContainer);
