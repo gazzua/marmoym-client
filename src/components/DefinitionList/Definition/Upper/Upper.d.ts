9507eecf0ae830e6d4df8d1c887b93e8326645fc
engine <enginehenryed@gmail.com>
2017-09-17 08:38:08 +0900

    Add common styled-component-wrapped HTML element to be used wide
    Refs: #50 
    PR: #51

---
+++ src/components/DefinitionList/Definition/Upper/Upper.d.ts
@@ -0,0 +1,13 @@
+import DefaultMobile from './Upper.mobile';
+import * as mobile from './Upper.mobile';
+// import DefaultWeb from './AddTerm.web';
+// import * as web from './AddTerm.web';
+
+declare var _test: typeof mobile;
+// declare var _test: typeof web;
+
+declare var _testDefault: typeof DefaultMobile;
+// declare var _testDefault: typeof DefaultWeb;
+
+export * from './Upper.mobile';
+export default DefaultMobile;
