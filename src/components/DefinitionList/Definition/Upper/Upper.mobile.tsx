616b5c641fc643b9b0840c9c3daa37984d6e01ca
enginehenryed <enginehenryed@gmail.com>
2017-09-18 01:55:13 +0900

    Add `UpperTop` and `UpperBottom` of `Definition`
    Refs: #49, #54
    font-awesome has also been added.

--- src/components/DefinitionList/Definition/Upper/Upper.mobile.tsx
+++ src/components/DefinitionList/Definition/Upper/Upper.mobile.tsx
@@ -5,12 +5,18 @@ import { resolveNames, withProps } from 'styles/utils';
 import { Div, Span } from 'styles/elem';
 import { border } from 'styles/debug';
 import BadgeList from 'components/BadgeList/BadgeList';
+import Star from 'components/common/Star/Star';
 
 const Meta = Div.extend``;
 const Term = Div.extend``;
 const Author = Span.extend``;
 const Time = Span.extend``;
 const UpperTop = Div.extend``;
+const UpperBottom = Div.extend``;
+const Roman = Span.extend``;
+const Pos = Span.extend``;
+const BottomMiddle = Div.extend``;
+const BottomRight = Div.extend``;
 
 const StyledUpper = Div.extend`
   ${border('black')}
@@ -26,7 +32,16 @@ const Upper = (props) => {
           <Time>3주전</Time>
         </Meta>
       </UpperTop>
-      <Term>{props.term}</Term>
+      <UpperBottom>
+        <Term>{props.term}</Term>
+        <BottomMiddle>
+          <Roman>roman</Roman>
+          <Pos>pos</Pos>
+        </BottomMiddle>
+        <BottomRight>
+          <Star/>
+        </BottomRight>
+      </UpperBottom>
     </StyledUpper>
   );
 };
--- src/containers/AppContainer/AppContainer.mobile.tsx
+++ src/containers/AppContainer/AppContainer.mobile.tsx
@@ -2,18 +2,12 @@ import * as React from 'react';
 import { injectGlobal } from 'styled-components';
 
 import globalStyle from 'styles/global';
-import MastheadContainer from 'containers/MastheadContainer/MastheadContainer';
-import PageContainer from 'containers/PageContainer/PageContainer';
 import App from 'components/App/App';
 
-interface AppContainerProps {
-    masthead?: typeof MastheadContainer;
-    pageContent?: any;
-}
-
-class AppContainer extends React.Component<AppContainerProps, {}> {
+class AppContainer extends React.Component {
   componentWillMount() {
     globalStyle();
+    require('font-awesome/css/font-awesome.css');
     console.log("app container will mount");
   }
 
