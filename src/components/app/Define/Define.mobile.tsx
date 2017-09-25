66b50f34170f824ba7292ca4517011c51c532c6c
Sungwon Choi <sungwon520@gmail.com>
2017-09-25 21:27:19 +0900

    Add more detail into `AddTerm` component
    Refs: #52
    PR: #53 
    
    - 용례를 기입할 수 있는 input tag 를 만듬.
    - 추가 버튼을 통해 동적으로 그 수가 늘어남.

--- src/components/app/Define/Define.mobile.tsx
+++ src/components/app/Define/Define.mobile.tsx
@@ -2,65 +2,74 @@ import * as React from 'react';
 import styled, { StyledFunction } from 'styled-components';
 
 import { withProps } from '@src/styles/utils';
+import { Div, Span, Input, P, Button } from '@src/styles/elem';
 
 const StyledDefine = styled.div`
   padding : 10px;  
-  input {
-    width: 100%;
-    height: 40px;
-    border : 1px solid black;
-  }
-  button {
-    width : 100%;
-    border : none;
-    height : 40px;
-    outline : none;
-    cursor : pointer;
-  }
 `;
+const Usage = Div.extend``;
+const UsageInput = Input.extend`
+  width: 90%;
+  height: 40px;
+  border : 1px solid black;
+`;
+const UsageLabel = P.extend``;
+const UsageAddButton = Button.extend`
+  width : 100%;
+  border : none;
+  height : 40px;
+  outline : none;
+  cursor : pointer;
+`;
+const UsageInputConatiner = Div.extend``;
+const UsageDeleteButton = Button.extend``;
 
 const renderUsage = (props) => {
   const UsageList = props.usages.map((usage, i) => (
-    <input
-      key={i}
-      defaultValue={usage}
-      placeholder="말의 쓰임새를 써주세요(ex: 실화니;이거실화임?)"/>
+    <UsageInputConatiner key={i}>
+      <UsageInput
+        defaultValue={usage}
+        placeholder="말의 쓰임새를 써주세요(ex: 실화니;이거실화임?)"/>
+      <UsageDeleteButton>X</UsageDeleteButton>
+    </UsageInputConatiner>
   ));
-
   return (
-    <div>
-      <p>쓰임새</p>
+    <Usage>
+      <UsageLabel>쓰임새</UsageLabel>
       {UsageList}
-      <button onClick={props.handleClickAddUsage}>+ 쓰임새 추가하기</button>
-    </div>  
+      <UsageAddButton onClick={props.handleClickAddUsage}>+ 쓰임새 추가하기</UsageAddButton>
+    </Usage>  
   );
 };
 
-const StyledTerm = styled.div``;
+const StyledTerm = Div.extend``;
+const TermLabel = P.extend``;
+const TermInput = Input.extend`
+  width: 100%;
+  height: 40px;
+  border : 1px solid black;
+`;
 
-const Term = (props) => (
-  <StyledTerm>
-    <p>올릴말</p>
-    <input type="text"/>
-  </StyledTerm>
-);
+const StyledDefinition = Div.extend``;
+const DefinitionLabel = P.extend``;
+const DefinitionInput = Input.extend`
+  width: 100%;
+  height: 40px;
+  border : 1px solid black;
+`;
 
-const StyledDefinition = styled.div``;
-
-const Definition = (props) => (
-  <StyledDefinition>
-    <div>
-      <p>뜻</p>
-      <input id=""/>
-    </div>
-  </StyledDefinition>
-);
 
 const Define = (props) => {
   return (
     <StyledDefine {...props}>
-      <Term/>
-      <Definition/>
+      <StyledTerm>
+        <TermLabel>올릴말</TermLabel>
+        <TermInput />
+      </StyledTerm>
+      <StyledDefinition>
+        <DefinitionLabel>뜻</DefinitionLabel>
+        <DefinitionInput />
+      </StyledDefinition>
       {renderUsage(props)}
     </StyledDefine>
   );
