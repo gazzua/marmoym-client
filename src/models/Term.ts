547b427a7824e34b03c417c65d16756afa672eea
UnoRich <wonyeong91@gmail.com>
2017-10-14 22:29:19 +0900

    Create Term Record and factory funtions
    Signed-off-by: UnoRich <wonyeong91@gmail.com>

---
+++ src/models/Term.ts
@@ -0,0 +1,54 @@
+import * as Immutable from 'immutable';
+import { TermType } from './ModelTypes';
+
+const TermRecord = Immutable.Record({
+  id: '0',
+  label: 'label',
+  roman: 'roman',
+  created_at: 0,
+  updated_at: 0
+});
+
+const Term = (function() {
+  function ofMany(terms) {
+    let newTerms = Immutable.Map();
+    for (const id in terms) {
+      newTerms = newTerms.set(
+        id.toString(), 
+        of(terms[id]));
+    }
+    return newTerms;
+  }
+  
+  function of(term) {
+    return new TermRecord({
+      id: term.id,
+      label: term.label,
+      roman: term.roman,
+      created_at: term.created_at,
+      updated_at: term.updated_at
+    })
+  }
+
+  function softMerge(terms) {
+    return {
+      into: function(Terms) {
+        let newTerms = Terms;
+        Terms.map(term => {
+          newTerms = newTerms.set(
+            term.id.toString(), 
+            Immutable.fromJS(term));
+        });
+        return newTerms;
+      }
+    }
+  }
+
+  return {
+    of,
+    ofMany,
+    softMerge
+  }
+})();
+
+export default Term;
