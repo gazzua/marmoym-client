c01e24b657553f82ecebe0c6711e320689d53288
enginehenryed <enginehenryed@gmail.com>
2017-10-15 01:35:54 +0900

    Add babel-plugin-styled-components and production script

--- internals/webpack/webpack.prod.mobile.config.js
+++ internals/webpack/webpack.prod.mobile.config.js
@@ -1,67 +1,11 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var merge = require('webpack-merge');
 
-var config = {
-  entry: './src/app.js',
-  output: {
-    filename: "bundle.js",
-    path: path.resolve('dist', 'bundle')
-  },
+var config = require('./webpack.prod.config.js');
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
+var _config = {
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", '.jsx']
-  },
-
-  plugins: [
-    new ExtractTextPlugin('../style.css')
-  ],
-
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: "source-map-loader"
-      },
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
-      },
-      {
-        test: /\.js$/, // Transform all .js files required somewhere with Babel
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          //resolve-url-loader may be chained before sass-loader if necessary
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
-  },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  },
+    extensions: ['.mobile.ts', '.mobile.tsx']
+  }
 };
 
-module.exports = config;
+module.exports = merge(config, _config);
---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+/**
+ * Test file
+ */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+
+const rootEl = document.getElementById('main');
+// const store = configureStore();
+const Div = styled.div``
+
+console.log(123, rootEl);
+
+ReactDOM.render(
+  <Div>123</Div>,
+  rootEl);
