777770783fb8c879eeab88e42f34c9a602933d07
enginehenryed <enginehenryed@gmail.com>
2017-10-18 02:21:04 +0900

    Remove .inner out of `Root` component

--- src/components/app/Root/Root.mobile.tsx
+++ src/components/app/Root/Root.mobile.tsx
@@ -6,24 +6,20 @@ import { Div } from '@styles/elems';
 import { withProps, resolveNames } from '@src/styles/styleUtils';
 
 const StyledRoot = styled.div`
-  ${media.w320(`
+  ${media.w320`
     min-width: 320px;
     height: 100%;
-    .${'inner'} {
-      width: 320px;
-      margin: 0 auto;
-      min-height: 100%;
-      ${styles.border('coral')}
-    }`)
-  }
+  `};
+
+  ${media.w480`
+    opacity: 0.5;  
+  `}
 `;
 
 const Root = (props) => {
   return (
     <StyledRoot>
-      <div className={'inner'}>
-        {props.children}
-      </div>
+      {props.children}
     </StyledRoot>
   );
 };
