3414b9b0a72542a7c121e46c27ee214ac84b1f8c
Engine S. Park <enginehenryed@gmail.com>
2017-10-18 02:22:53 +0900

    Remove width 320 border, which is no longer necessary
    * Make media.ts export default object that has various functions
    * Remove .inner out of `Root` component
    
    PR-URL: https://github.com/tymsai/marmoym-client/pull/117

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -1,6 +1,16 @@
-export const w320 = (content) => {
-  return `
-    @media (max-width: 479px) {
-      ${content}
-    }`;
+import { css } from 'styled-components'; 
+
+export default {
+  w320: (content) => {
+    return css`
+      @media (max-width: 479px) {
+        ${css(content)}
+      }`
+  },
+  w480: (content) => {
+    return css`
+    @media (min-width: 480px) {
+      ${css(content)}
+    }`
+  }
 }
