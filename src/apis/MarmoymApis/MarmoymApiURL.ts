8baa2dd56568d08ead4b41f17ee2dfcb2d66504c
enginehenryed <enginehenryed@gmail.com>
2017-10-22 00:31:57 +0900

    Add search in MarmoymURLs

--- src/apis/MarmoymApis/MarmoymApiURL.ts
+++ src/apis/MarmoymApis/MarmoymApiURL.ts
@@ -4,3 +4,5 @@ const API_ROOT = `http://localhost:4001/api/v1`;
 export const GET_DEFINITION_IDS = `${API_ROOT}/definitions/ids`
 export const GET_DEFINITIONS = `${API_ROOT}/definitions`;
 export const ADD_DEFINITION = `${API_ROOT}/newdefinitions`;
+
+export const SEARCH = `${API_ROOT}/search`;
