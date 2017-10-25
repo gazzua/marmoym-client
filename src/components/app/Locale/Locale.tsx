1fd351cc2cfa79eb58cfbe05a80661fa36f0ae56
enginehenryed <enginehenryed@gmail.com>
2017-10-25 20:33:12 +0900

    Remove use of `lodash` in `Locale`

--- src/components/app/Locale/Locale.tsx
+++ src/components/app/Locale/Locale.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled, { StyledFunction } from 'styled-components';
 import { connect } from 'react-redux';
-import * as _ from 'lodash';
 
 import { selectLocale } from '@selectors/localeSelector';
 
@@ -13,7 +12,7 @@ const mapStateToProps = (state) => {
 
 const Locale = connect(mapStateToProps)((props) => {
   // console.log("translate", props.label, props.trans);
-  const label = _.get(props.locale, [props.label])
+  const label = props.locale[props.label];
 
   return (
     <span className={props.className}>
