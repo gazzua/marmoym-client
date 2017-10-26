28920a6ad91acd4453dec051445ae1b297e41396
enginehenryed <enginehenryed@gmail.com>
2017-10-27 00:33:58 +0900

    Create `AppURL` that has all the predefined URLs for app
    AppURL also has build() which builds actual url string with given arguments

---
+++ src/models/AppURL.ts
@@ -0,0 +1,15 @@
+export const ROOT = `/`;
+export const SEARCH = `/search`;
+export const TERMS = `/terms`;
+export const DEFINITIONS_ID = `/definitions/:defId`;
+export const DEFINE = `/define`;
+export const SIGNIN = `/signin`;
+
+export function build(URL: string, ...tokens) {
+  switch (URL) {
+    case DEFINITIONS_ID:
+    return `/definitions/${tokens[0]}`;
+    default:
+    return null;
+  }
+}
