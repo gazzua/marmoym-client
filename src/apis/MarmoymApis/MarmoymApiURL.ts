6fe9645af12b980423d0598b26ec2d3265e75bbb
enginehenryed <enginehenryed@gmail.com>
2017-11-10 02:53:57 +0900

    Replace back-ticks in marmoymApiURL with single-quote

--- src/apis/MarmoymApis/MarmoymApiURL.ts
+++ src/apis/MarmoymApis/MarmoymApiURL.ts
@@ -1,5 +1,5 @@
 // const API_ROOT = 'http://marmoym.com:4001/api/v1';
-const API_ROOT = `http://localhost:4001/api/v1`;
+const API_ROOT = 'http://localhost:4001/api/v1';
 
 export const GET_DEFINITIONS_IDS = `${API_ROOT}/definitions/ids`;
 export const GET_DEFINITIONS = `${API_ROOT}/definitions`;
