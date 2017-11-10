8aba00f40508a48a1beb1f190acb2e617ff48068
Engine S. Park <enginehenryed@gmail.com>
2017-11-10 20:40:38 +0900

    Correct variable name `ApiURL.DEFINITION.IDS`
    PR-URL: https://github.com/tymsai/marmoym-client/pull/174

--- src/apis/MarmoymApis/MarmoymApiURL.ts
+++ src/apis/MarmoymApis/MarmoymApiURL.ts
@@ -1,7 +1,7 @@
 // const API_ROOT = 'http://marmoym.com:4001/api/v1';
 const API_ROOT = `http://localhost:4001/api/v1`;
 
-export const GET_DEFINITION_IDS = `${API_ROOT}/definitions/ids`;
+export const GET_DEFINITIONS_IDS = `${API_ROOT}/definitions/ids`;
 export const GET_DEFINITIONS = `${API_ROOT}/definitions`;
 export const ADD_DEFINITION = `${API_ROOT}/newdefinitions`;
 
