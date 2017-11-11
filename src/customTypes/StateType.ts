d0584d725ec1ebcb88f1ce7dba75506a599c5316
enginehenryed <enginehenryed@gmail.com>
2017-11-11 19:13:44 +0900

    Create `customTypes/` containing type definitions

---
+++ src/customTypes/StateType.ts
@@ -0,0 +1,13 @@
+import * as Immutable from 'immutable';
+
+export interface DefinitionType {
+  definitions?: any;
+}
+
+export interface TermType {
+  terms?: any;
+}
+
+export interface DefinitionStateType {
+  
+}
