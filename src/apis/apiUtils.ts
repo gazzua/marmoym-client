592666d02ce1fa1e356087f0be5369598d24e50a
enginehenryed <enginehenryed@gmail.com>
2017-11-19 14:52:52 +0900

    Modify payload selector to handle error response

--- src/apis/apiUtils.ts
+++ src/apis/apiUtils.ts
@@ -1,7 +1,17 @@
+import ErrorBase from '@models/Error/ErrorBase';
+
 export function getData(res) {
   return res.data;
 }
 
-export function getPayload(res) {
-  return res.data.payload;
+export function createPayloadSelector(Error: typeof ErrorBase) {
+  return (res) => {
+    if (res.data.code !== 200000) {
+      throw new Error({
+        code: res.data.code,
+        msg: res.data.msg,
+      });
+    }
+    return res.data.payload;
+  };
 }
